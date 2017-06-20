579dd1674f0ef9ab083b6da8bbf8f629536b78b8
BrianBravoski <Brianbravoski28@gmail.com>
2017-06-20 22:42:30 +0300

    the navbar is functional

--- js/Anita.js
+++ js/Anita.js
@@ -1,16 +1,9 @@
-// the function which handles the input field logic
-function getUserName() {
-    var eMail = document.getElementById('email').value;
-    var result = alert(result);
-
-    if (eMail.length < 3) {
-        result.textContent = 'e-mail must contain at least 3 characters';
-        //alert('Username must contain at least 3 characters');
-    } else {
-        result.textContent =eMail + " has been successfully added to our e-mail list. Thank you."
-        //alert(nameField);
-    }
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
 }
-// use an eventlistener for the event
-var button= document.getElementById('button');
-button.addEventListener('click', getUserName, false);
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
